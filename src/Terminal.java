80fce6b56771f95ed7fb15760a651e75b6a90f8e
Gygain <gygain@gmail.com>
2022-04-13 14:08:07 -0600

    Removed comment

--- src/Terminal.java
+++ src/Terminal.java
@@ -7,8 +7,8 @@ public class Terminal {
     public static void main(String[] args) throws FileNotFoundException  {
 
         title();
-        //TerminalWindow tw = new TerminalWindow();
-        //tw.LoginWindow();
+        TerminalWindow tw = new TerminalWindow();
+        tw.LoginWindow();
         
         POSProxy prox = new POSProxy();
 
