d1b28f80639cea5400c552e77281f8e9aca9e4cc
Gygain <gygain@gmail.com>
2022-04-13 15:17:01 -0600

    Database tracks time in
    Database now tracks the number of seconds you are logged in for

--- src/Database.java
+++ src/Database.java
@@ -9,14 +9,14 @@ import java.util.Scanner;
 
 public class Database {
 	private static Database instance;
-	private static String[][] database = new String[10][6]; //max 10 users currently
+	private static String[][] database = new String[10][8]; //max 10 users currently
 	private static String filePath = "";
 	
 	private Database(String file) throws FileNotFoundException {
 		filePath = file;
 				
 		String line = ""; // line of the file
-		String[] lineData = new String[6]; // holds the data from an individual line		
+		String[] lineData = new String[8]; // holds the data from an individual line		
 		
 		//File to DB
 		Scanner input = new Scanner(new File(filePath));
@@ -30,7 +30,7 @@ public class Database {
 			int index = Integer.parseInt(lineData[0]);
 			database[index][0] = lineData[0];
 			
-			for(int i = 1; i < 6; i++) {
+			for(int i = 1; i < 8; i++) {
 				database[index][i] = lineData[i];
 			}
 		}
@@ -54,7 +54,7 @@ public class Database {
 		PrintStream fout = new PrintStream(new FileOutputStream(filePath));
 		fout.print("id,num,name,pass,perms,loggedin\n");
 		for(int i = 0; i < 10; i++) {
-			for(int j = 0; j < 6; j++) {
+			for(int j = 0; j < 8; j++) {
 				fout.print(database[i][j] + ",");
 			}
 			fout.print("\n");
@@ -73,6 +73,41 @@ public class Database {
 		return false;
 	}
 	
+	//Name from id #
+	public String getName(int id) {
+		for(int i = 0; i < 10; i++) {
+			if(Integer.parseInt(database[i][1]) != id) {
+				continue;
+			}
+			return database[i][2];
+		}
+		return null;
+	}
+	
+	
+	public String getTime(int id) {
+		for(int i = 0; i < 10; i++) {
+			if(Integer.parseInt(database[i][1]) != id) {
+				continue;
+			}
+			return database[i][7];
+		}
+		return null;
+	}
+	
+	
+	//used for deleting, check perms somewhere else.
+	private int getRowNum(int id) {
+		for(int i = 0; i < 10; i++) {
+			if(Integer.parseInt(database[i][1]) != id) {
+				continue;
+			}
+			return i;
+		}
+		return -1;
+	}
+	
+	
 	//Logs user in, given id # and password
 	//-1 = bad pass, 0 = logged in already, 1 = log in
 	public int login(int id, String hashedPass) {
@@ -81,6 +116,7 @@ public class Database {
 			if(hashedPass.equals(database[i][3])) {
 				if(database[i][5].equals("false")) {
 					database[i][5] = "true";
+					database[i][6] = java.time.Instant.now().getEpochSecond() + "";
 					return 1;
 				} else { //already logged in
 					return 0;
@@ -98,6 +134,8 @@ public class Database {
 			if(hashedPass.equals(database[i][3])) {
 				if(database[i][5].equals("true")) {
 					database[i][5] = "false";
+					database[i][7] = (java.time.Instant.now().getEpochSecond() - Long.parseLong(database[i][6])) + "";
+					database[i][6] = "0";
 					return 1;
 				} else { //already logged out
 					return 0;
@@ -107,35 +145,12 @@ public class Database {
 		return -1;
 	}
 	
-	//Name from id #
-	public String getName(int id) {
-		for(int i = 0; i < 10; i++) {
-			if(Integer.parseInt(database[i][1]) != id) {
-				continue;
-			}
-			return database[i][2];
-		}
-		return null;
-	}
-	
-	
-	//used for deleting, check perms somewhere else.
-	private int getRowNum(int id) {
-		for(int i = 0; i < 10; i++) {
-			if(Integer.parseInt(database[i][1]) != id) {
-				continue;
-			}
-			return i;
-		}
-		return -1;
-	}
-	
 	//Deletes a user from the database given their user id
 	//true - deleted, false - no one to delete
 	public boolean deleteUser(int id) throws FileNotFoundException {
 		int row = getRowNum(id);
 		if(row != -1) { //user exists
-			for(int i = 1; i < 6; i++) {
+			for(int i = 1; i < 8; i++) {
 				database[row][i] = "-1";
 			}
 			instance.update();
@@ -156,7 +171,7 @@ public class Database {
 				
 				database[i][0] = i + ""; //set row num
 				
-				for(int j = 0; j < 5; j++) { //fill rest of info
+				for(int j = 0; j < 7; j++) { //fill rest of info
 					database[i][j+1] = info[j]; 
 				}
 				instance.update();
