575b02ee58b2b42445749386dd021e36a9cb1093
Gygain <gygain@gmail.com>
2022-04-17 23:09:15 -0600

    Added Java Doc to Database
    Added Java Doc comments to Database

--- src/Database.java
+++ src/Database.java
@@ -7,25 +7,25 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Database extends Service{
-	private static Database instance;
-	private static String[][] database = new String[10][8]; //max 10 users currently
-	private static String filePath = "";
-	private static int highestID = 0;
-	protected static int currentUserID = -1;
+	private static Database instance; // Represents database
+	private static String[][] database = new String[10][8]; //max 10 users
+	private static String filePath = ""; // Filepath to CSV of database
+	private static int highestID = 0; // Highest user ID in use
+	protected static int currentUserID = -1; // User ID currently logged in
 	
 	private Database(String file) throws FileNotFoundException {
 		filePath = file;
 				
 		String line = ""; // line of the file
-		String[] lineData = new String[8]; // holds the data from an individual line		
+		String[] lineData = new String[8]; // data from an individual line		
 		
-		//File to DB
+		// Take data from CSV and put it in Database object
 		Scanner input = new Scanner(new File(filePath));
 		line = input.nextLine(); // clear header line
 		
 		while (input.hasNextLine()) {
 
-			// gets the line and splits it into the array
+			// get the line and split it into the array
 			line = input.nextLine();
 			lineData = line.split(",");
 			int index = Integer.parseInt(lineData[0]);
@@ -47,6 +47,13 @@ public class Database extends Service{
 		}
 	}
 	
+	
+    /**
+     * Get instance of the Database
+     * @param file - path to file of data for the database
+     * @return current instance of the database
+     * @throws FileNotFoundException
+     */
 	protected static Database getInstance(String file) throws FileNotFoundException {
         if (instance == null) {
             instance = new Database(file);
@@ -54,6 +61,12 @@ public class Database extends Service{
         return instance;
     }
 
+	
+    /**
+     * Get instance of the Database
+     * @return current instance of the database
+     * @throws FileNotFoundException
+     */
 	protected static Database getInstance(){
 		if(instance != null){
 			return instance;
@@ -62,16 +75,22 @@ public class Database extends Service{
 			return null;
 		}
 	}
-
-	private static Boolean findUser(String name) {
-		return false;
-	}
-
+	
+	
+    /**
+     * Print formatted Database data
+     * @return UNUSED
+     */
 	private void print() {
 		System.out.println(Arrays.deepToString(database).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
 	}
 	
-	//Check if a user exists by user id
+	
+    /**
+     * Check if a user exists by user ID
+     * @param id - user to check
+     * @return boolean - true if user exists in the database
+     */
 	public static boolean userExists(int id) {
 		for(int i = 0; i < 10; i++) {
 			if(Integer.parseInt(database[i][1]) == id) {
@@ -81,7 +100,12 @@ public class Database extends Service{
 		return false;
 	}
 	
-	//Name from id #
+	
+    /**
+     * Get a user's name from their ID
+     * @param id - user to get the name of
+     * @return user's name; null if user does not exist
+     */
 	private String getName(int id) {
 		for(int i = 0; i < 10; i++) {
 			if(Integer.parseInt(database[i][1]) != id) { continue; }
@@ -90,7 +114,13 @@ public class Database extends Service{
 		return null;
 	}
 	
-	//Get time clocked in for (seconds)
+	
+    /**
+     * Get the time a given user was logged in for
+     * @param id - user to get time of
+     * @return the number of seconds the user was logged in for;
+     * 		   null if user does not exist
+     */
 	private String getTime(int id) {
 		for(int i = 0; i < 10; i++) {
 			if(Integer.parseInt(database[i][1]) != id) { continue; }
@@ -99,7 +129,12 @@ public class Database extends Service{
 		return null;
 	}
 	
-	//Get permissions for a given user
+	
+    /**
+     * Get permissions for a given user
+     * @param if - user to get permissions of
+     * @return string of permissions the user has; null if user does not exist
+     */
 	protected String getPerms(int id) {
 		for(int i = 0; i < 10; i++) {
 			if(Integer.parseInt(database[i][1]) != id) { continue; }
@@ -109,7 +144,10 @@ public class Database extends Service{
 	}
 	
 	
-	//Returns whether someone is currently logged in
+    /**
+     * Get whether someone is currently logged in
+     * @return boolean - true if someone is logged in
+     */
 	private static boolean loggedIn() {
 		for(int i = 0; i < 10; i++) {
 			if(database[i][5].equals("true")) {
@@ -119,7 +157,12 @@ public class Database extends Service{
 		return false;
 	}
 		
-	//Used for deleting users
+
+	/**
+     * Get row in the database of a given user. Used when deleting users.
+     * @param id - user id to get the row of
+     * @return row number; -1 if user does not exist
+     */
 	private int getRowNum(int id) {
 		for(int i = 0; i < 10; i++) {
 			if(Integer.parseInt(database[i][1]) != id) {
@@ -130,8 +173,11 @@ public class Database extends Service{
 		return -1;
 	}
 	
-	//Update database file to current state of database object
-	//Called automatically after adding / deleting a user
+	
+    /**
+     * Update the database CSV to the current state of the database object.
+     * @return UNUSED
+     */
 	private void update() throws FileNotFoundException {
 		PrintStream fout = new PrintStream(new FileOutputStream(filePath));
 		fout.print("id,num,name,pass,perms,loggedin\n");
@@ -144,8 +190,15 @@ public class Database extends Service{
 		fout.close();
 	}
 	
-	//Logs user in, given id # and password
-	//-2 someone is logged in, -1 = bad pass, 0 = logged in already, 1 = log in
+	
+    /**
+     * Logs a user into the system
+     * @param id - user to log in
+     * @param hashedPass - hashed user's password
+     * @return 1 - successfully logged in; 0 - already logged in; -1 incorrect password;
+     * 		   -2 someone else is already logged in
+     * @throws FileNotFoundException
+     */
 	protected static int login(int id, String hashedPass) throws FileNotFoundException {
 		if(loggedIn()) { return -2; }
 		for(int i = 0; i < 10; i++) {
@@ -165,8 +218,15 @@ public class Database extends Service{
 		return -1;
 	}
 	
-	//Logs user out, given id # and password
-	//-2 = no one is logged in, -1 = bad pass, 0 = logged out already, 1 = log out
+	
+    /**
+     * Logs a user out of the system
+     * @param id - user to log out
+     * @param hashedPass - hashed user's password
+     * @return 1 - successfully logged out; 0 - already logged out; -1 incorrect password;
+     * 		   -2 no one is logged in
+     * @throws FileNotFoundException
+     */
 	protected static int logout(int id, String hashedPass) throws FileNotFoundException {
 		if(!loggedIn()) { return -2; }
 		for(int i = 0; i < 10; i++) {
@@ -187,8 +247,13 @@ public class Database extends Service{
 		return -1;
 	}
 	
-	//Deletes a user from the database given their user id
-	//returns true on success
+
+    /**
+     * Deletes a user from the database
+     * @param id - user to be deleted
+     * @return boolean - success on deletion
+     * @throws FileNotFoundException
+     */
 	protected boolean deleteUser(int id) throws FileNotFoundException {
 		int row = getRowNum(id);
 		if(row != -1) { //user exists
@@ -201,8 +266,13 @@ public class Database extends Service{
 		return false;
 	}
 	
-	//Adds a user to the database given their info in an array
-	//of strings. Returns true on success
+	
+    /**
+     * Adds a user to the database
+     * @param info - name, hashed password, permissions
+     * @return boolean - success on addition
+     * @throws FileNotFoundException
+     */
 	protected boolean addUser(String[] info) throws FileNotFoundException {
 		for(int i = 0; i < 10; i++) { //find an empty row to insert into
 			if(database[i][1] != "-1") {continue;}
@@ -220,7 +290,13 @@ public class Database extends Service{
 	}
 
 	
-	//Adds a permission to a given user. Returns true on success
+    /**
+     * Add a permission to a given user
+     * @param id - user to add permission to
+     * @param perm - permission to add 
+     * @return boolean - success addition
+     * @throws FileNotFoundException
+     */
 	protected boolean addPerm(int id, int perm) throws FileNotFoundException {
 		for(int i = 0; i < 10; i++) { 
 			if(Integer.parseInt(database[i][1]) != id) { continue; }
@@ -233,7 +309,14 @@ public class Database extends Service{
 		return false;
 	}
 	
-	//Removes a permission from a given user. Returns true on success
+	
+    /**
+     * Remove a permission from a given user
+     * @param id - user to remove permission from
+     * @param perm - permission to remove 
+     * @return boolean - success removal
+     * @throws FileNotFoundException
+     */
 	protected boolean removePerm(int id, int perm) throws FileNotFoundException {
 		for(int i = 0; i < 10; i++) { 
 			if(Integer.parseInt(database[i][1]) != id) { continue; }
